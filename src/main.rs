6a1cd8472da2fb655b198d870c9b257a2e623685
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2020-11-12 20:29:18 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use std::io;
+use std::collections;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+#[derive(From)]
+enum ActionType {
+    Cast,
+    OpponentCast,
+    Learn,
+    Brew,
+}
+
+struct Action {
+    id: i32,
+    action_type: ActionType,
+    tiers_0_ingredient: i32,
+    tiers_1_ingredient: i32,
+    tiers_2_ingredient: i32,
+    tiers_3_ingredient: i32,
+    price: i32,
+    tome_index: i32,
+    tax_count: i32,
+    castable: i32,
+    repeatable: i32,
+}
+
+struct Player {
+    tiers_0_inventory: i23,
+    tiers_1_inventory: i23,
+    tiers_2_inventory: i23,
+    tiers_3_inventory: i23,
+    score: i32,
+}
+
+fn main() {
+
+    // game loop
+    loop {
+        let mut brewings: Vec<Action> = Vec::new();
+        let mut players: [Player; 2] = Default::default();
+
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let action_count = parse_input!(input_line, i32); // the number of spells and recipes in play
+        for i in 0..action_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let id = parse_input!(inputs[0], i32); // the unique ID of this spell or recipe
+            let action_type = inputs[1].trim().to_string(); // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
+            let tiers_0_ingredient = parse_input!(inputs[2], i32); // tier-0 ingredient change
+            let tiers_1_ingredient = parse_input!(inputs[3], i32); // tier-1 ingredient change
+            let tiers_2_ingredient = parse_input!(inputs[4], i32); // tier-2 ingredient change
+            let tiers_3_ingredient = parse_input!(inputs[5], i32); // tier-3 ingredient change
+            let price = parse_input!(inputs[6], i32); // the price in rupees if this is a potion
+            let tome_index = parse_input!(inputs[7], i32); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax
+            let tax_count = parse_input!(inputs[8], i32); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
+            let castable = parse_input!(inputs[9], i32); // in the first league: always 0; later: 1 if this is a castable player spell
+            let repeatable = parse_input!(inputs[10], i32); // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+
+            match action_type {
+                String::from("BREW") =>
+                    brewings.push(Point {
+                        id,
+                        action_type,
+                        tiers_0_ingredient,
+                        tiers_1_ingredient,
+                        tiers_2_ingredient,
+                        tiers_3_ingredient,
+                        price,
+                        tome_index,
+                        tax_count,
+                        castable,
+                        repeatable,
+                    }),
+                _ => eprintln!("oops")
+            }
+        }
+        for i in 0..2 as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let tiers_0_inventory = parse_input!(inputs[0], i32); // tier-0 ingredients in inventory
+            let tiers_1_inventory = parse_input!(inputs[1], i32);
+            let tiers_2_inventory = parse_input!(inputs[2], i32);
+            let tiers_3_inventory = parse_input!(inputs[3], i32);
+            let score = parse_input!(inputs[4], i32); // amount of rupees
+            players[i] = Player {
+                tiers_0_inventory,
+                tiers_1_inventory,
+                tiers_2_inventory,
+                tiers_3_inventory,
+                score,
+            };
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+
+        // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
+        println!("BREW 0");
+    }
+
+    fn get_best_potion(players: array<Player>, brewing: Vec<Action>) {
+        brewing.iter().max_by()
+    }
+}
